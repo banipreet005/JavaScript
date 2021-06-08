1d0022609ebc4f66cdb667e403c7eee0cf579c93
Banipreet Singh <bansingh@adobe.com>
2021-06-08 08:37:16 -0700

    pushing html and js for app

---
+++ todo-app/main.js
@@ -0,0 +1,32 @@
+let form = document.querySelector('form');
+let todoList = document.querySelector('ul');
+let button = document.querySelector('button');
+let input = document.getElementById('user-todo-list');
+let todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+
+localStorage.setItem('todos', JSON.stringify(todosArray));
+
+let storage = JSON.parse(localStorage.getItem('todos'));
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    todosArray.push(input.value);
+    localStorage.setItem('todos', JSON.stringify(todosArray));
+    todoMaker(input.value);
+    input.value = '';
+})
+
+let todoMaker = function (text) {
+    let todo = document.createElement('li');
+    todo.textContent = text;
+    todoList.appendChild(todo);
+}
+
+storage.forEach(elem => todoMaker(elem));
+
+button.addEventListener('click', function () {
+    localStorage.clear();
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.firstChild);
+    }
+});
